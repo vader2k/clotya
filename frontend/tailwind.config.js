c96eab8049a6809acb3f74ce09b9b8e838f1c31d
danie odukwe <kingdanny295@gmail.com>
2023-12-30 12:00:42 -0800

    succefully set up tailwind and installed react icons package

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: ["./index.css","./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: "480px",
+        ss: "620px",
+        sm: "768px",
+        md: "1060px",
+        lg: "1200px",
+        xl: "1700px",
+      },
+    },
   },
   plugins: [],
 }
-
