78832311cdc7325d134bb0b08aa5429407eff6d9
danie odukwe <kingdanny295@gmail.com>
2023-12-30 12:25:56 -0800

    created main components

---
+++ frontend/src/components/FeaturedProducts.jsx
@@ -0,0 +1,10 @@
+
+const FeaturedProducts = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FeaturedProducts
---
+++ frontend/src/components/HeroSlider.jsx
@@ -0,0 +1,10 @@
+
+const HeroSlider = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HeroSlider
