7eb7cf548b6421a22c8198915893f59d54de0e99
danie odukwe <kingdanny295@gmail.com>
2024-01-02 02:31:58 -0800

    figured a way to get the images to change

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -5,7 +5,11 @@ import women from '../assets/banner-1.jpg'
 import men from '../assets/banner-2.jpg'
 import shoe from '../assets/banner-3.jpg'
 import accessory from '../assets/banner-4.jpg'
+import basic1 from '../assets/basic1.jpeg'
+import basic2 from '../assets/basic2.jpeg'
+import basic3 from '../assets/basic3.jpeg'
+import basic4 from '../assets/basic4.jpeg'
 
 
 
-export { slide1, slide2, slide3, women, men, shoe, accessory } 
+export { slide1, slide2, slide3, women, men, shoe, accessory, basic1, basic2, basic3, basic4 } 
--- frontend/src/components/FeaturedProducts.jsx
+++ frontend/src/components/FeaturedProducts.jsx
@@ -1,8 +1,46 @@
+import { basic1, basic2, basic3, basic4 } from "../assets"
+import { useState } from "react";
 
 const FeaturedProducts = () => {
+
+  const data = [
+    basic1, basic2, basic3, basic4
+  ]
+  const [currentImage, setCurrentImage] = useState(0);
+
+  const handleHover = (percentage) => {
+    // Calculate the new picture index based on the percentage.
+    const totalImages = data.length;
+    const newIndex = Math.floor((percentage / 100) * totalImages) % totalImages;
+
+    // Set the new picture as the current picture.
+    setCurrentImage(newIndex);
+  };
+
+
   return (
-    <div>
-      fewatu
+    <div className="py-8">
+      <div className="flex justify-between">
+        <h1 className="text-[1.8rem] font-medium">Featured Products</h1>
+        <p className="max-w-[730px] text-gray-500 font-extralight">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
+      </div>
+
+      <div className="w-[300px] h-[600px] bg-gray-200 relative"
+        onMouseEnter={(e) => {
+          // Get the width of the box.
+          const boxWidth = e.target.offsetWidth;
+            // Calculate the percentage based on where you hover.
+          const hoverPercentage = (e.nativeEvent.offsetX / boxWidth) * 100;
+            // Call the handleHover function with this percentage.
+          handleHover(hoverPercentage);
+        }}>
+    
+        <img
+          src={data[currentImage]}
+          alt={`Image ${currentImage + 1}`}
+          className="w-full h-full object-cover"
+        />
+      </div>
     </div>
   )
 }
