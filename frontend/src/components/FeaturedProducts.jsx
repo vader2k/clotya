407235d69b51c0cb5b5ad823f7862189a0eb857a
danie odukwe <kingdanny295@gmail.com>
2024-01-03 11:48:49 +0100

    successfully did the details

--- frontend/src/components/FeaturedProducts.jsx
+++ frontend/src/components/FeaturedProducts.jsx
@@ -3,7 +3,7 @@ import { FeaturedData1, FeaturedData2, FeaturedData3 } from "../constants";
 import { Pagination, A11y } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper-bundle.css';
-
+import { FaStar } from "react-icons/fa";
 
 const FeaturedProducts = () => {
 
@@ -44,7 +44,7 @@ const FeaturedProducts = () => {
           onSwiper={(swiper) => console.log(swiper)}
         >
           <SwiperSlide>
-            <div className="py-8 flex overflow-x-auto gap-8 h-[700px] holder relative">
+            <div className="py-8 flex overflow-x-auto gap-8 h-[750px] holder relative">
             {FeaturedData1.map((item) => (
               <div
                 key={item.id}
@@ -56,12 +56,21 @@ const FeaturedProducts = () => {
                   src={item[`img${activeImageIndices[item.id] || 1}`]}
                   alt={`product-${item.id}`}
                 />
-              </div>
+
+                <div className="absolute bg-white p-5 px-6 w-[345px] bottom-[10px] left-[20px] flex flex-col gap-3 h-[160px]">
+                  <div className="flex  items-center gap-3"><FaStar className="text-[0.8rem] text-yellow-400"/> <span className="text-[0.8rem] font-medium">{item?.review}</span></div>
+                  <p className="text-[0.8rem] capitalize">{item?.name}</p>
+                  <div className="flex items-center gap-5 text-[0.9rem]">
+                    <span className="text-gray-400 line-through">{item?.oldPrice}</span>
+                    <span>{item?.price}</span>
+                  </div>
+                </div>
+              </div> 
             ))}
             </div>
           </SwiperSlide>
           <SwiperSlide>
-          <div className="py-8 flex overflow-x-auto gap-8 h-[700px] holder relative">
+          <div className="py-8 flex overflow-x-auto gap-8 h-[750px] holder relative">
             {FeaturedData2.map((item) => (
               <div
                 key={item.id}
@@ -73,12 +82,20 @@ const FeaturedProducts = () => {
                   src={item[`img${activeImageIndices[item.id] || 1}`]}
                   alt={`product-${item.id}`}
                 />
+                <div className="absolute bg-white p-5 px-6 w-[345px] bottom-[10px] left-[20px] flex flex-col gap-3 h-[160px]">
+                  <div className="flex  items-center gap-3"><FaStar className="text-[0.8rem] text-yellow-400"/> <span className="text-[0.8rem] font-medium">{item?.review}</span></div>
+                  <p className="text-[0.8rem] capitalize">{item?.name}</p>
+                  <div className="flex items-center gap-5 text-[0.9rem]">
+                    <span className="text-gray-400 line-through">{item?.oldPrice}</span>
+                    <span>{item?.price}</span>
+                  </div>
+                </div>
               </div>
             ))}
             </div>
           </SwiperSlide>
           <SwiperSlide>
-          <div className="py-8 flex overflow-x-auto gap-8 h-[700px] holder relative">
+          <div className="py-8 flex overflow-x-auto gap-8 h-[750px] holder relative">
             {FeaturedData3.map((item) => (
               <div
                 key={item.id}
@@ -90,6 +107,14 @@ const FeaturedProducts = () => {
                   src={item[`img${activeImageIndices[item.id] || 1}`]}
                   alt={`product-${item.id}`}
                 />
+                <div className="absolute bg-white p-5 px-6 w-[345px] bottom-[10px] left-[20px] flex flex-col gap-3 h-[160px]">
+                  <div className="flex  items-center gap-3"><FaStar className="text-[0.8rem] text-yellow-400"/> <span className="text-[0.8rem] font-medium">{item?.review}</span></div>
+                  <p className="text-[0.8rem] capitalize">{item?.name}</p>
+                  <div className="flex items-center gap-5 text-[0.9rem]">
+                    <span className="text-gray-400 line-through">{item?.oldPrice}</span>
+                    <span>{item?.price}</span>
+                  </div>
+                </div>
               </div>
             ))}
             </div>
