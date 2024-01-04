1823cd15aff03d499d127300407f623f86249bd9
danie odukwe <kingdanny295@gmail.com>
2024-01-04 14:46:38 +0100

    added the images for news component

--- frontend/src/components/News.jsx
+++ frontend/src/components/News.jsx
@@ -1,8 +1,16 @@
 
+
 const News = () => {
   return (
-    <div>
-      news
+    <div className="py-20">
+      <div className="text-center flex flex-col gap-3 pb-10">
+        <h1>Our Latest News</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra vel facilisis.</p>
+      </div>
+
+      <div>
+        
+      </div>
     </div>
   )
 }
