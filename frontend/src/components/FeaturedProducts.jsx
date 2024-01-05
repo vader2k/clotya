6e965352701e7ec854d1050c30fa543ee7e38c2f
danie odukwe <kingdanny295@gmail.com>
2024-01-05 17:57:52 +0100

    maybe its best to leave responsiveness for another time

--- frontend/src/components/FeaturedProducts.jsx
+++ frontend/src/components/FeaturedProducts.jsx
@@ -32,12 +32,12 @@ const FeaturedProducts = () => {
   };
 
   return (
-    <div className="py-8 relative ">
+    <div className="py-8 xxs:py-4 relative ">
       <div className="flex justify-between ">
-        <h1 className="text-[1.8rem] font-medium">Featured Products</h1>
-        <p className="max-w-[730px] text-gray-500 font-extralight">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
+        <h1 className="md:text-[1.8rem] font-medium">Featured Products</h1>
+        <p className="max-w-[730px]  text-gray-500 font-extralight">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
       </div>
-      <div className="relative">
+      <div className="relative ">
         <Swiper
           // install Swiper modules
           modules={[Pagination, A11y]}
@@ -153,7 +153,7 @@ const FeaturedProducts = () => {
                     <span>{item?.price}</span>
                   </div>
                 </div>
-                <div className="absolute top-4 left-2  px-2 py-1 text-[0.7rem] bg-white text-green-500 font-medium">
+                <div className="absolute top-4  left-2  px-2 py-1 text-[0.7rem] bg-white text-green-500 font-medium">
                   {item?.tag}
                 </div>
               </div>
