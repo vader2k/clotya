3c888733f748c20df60b45c55233a99f456c41e4
danie odukwe <kingdanny295@gmail.com>
2024-01-05 16:30:23 +0100

    dont with the contact section

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -5,22 +5,22 @@ const Contact = () => {
   return (
     <div className="bg-black text-white w-full p-20">
       <div className="max-w-[1300px] flex items-center justify-between m-auto">
-        <div>
-          <h1>Get our emails for info on new items, sales and more.</h1>
-          <p>We'll email you a voucher worth £10 off your first order over $50</p>
+        <div className="">
+          <h1 className="text-[1.5rem] font-bold max-w-[350px]">Get our emails for info on new items, sales and more.</h1>
+          <p className="pt-4 pb-8 text-[0.85rem] text-gray-400">We'll email you a voucher worth £10 off your first order over $50</p>
           <div className="flex items-center text-[0.9rem]">
             <input type="text" 
             placeholder="Enter your email address"
-            className="p-3 outline-none border-none text-gray-500 "/>
-            <button className="cursor-pointer text-white bg-black hover:bg-gray-600">Subscribe</button>
+            className="p-3 outline-none border-none text-gray-500  h-[50px] w-[55%]"/>
+            <button className="cursor-pointer text-white bg-black hover:bg-gray-600 py-3 px-5 h-[50px] border border-gray-300">Subscribe</button>
           </div>
-          <p>By subscribing you agree to our Terms & Conditions and Privacy & Cookies Policy.</p>
+          <p className="py-4 text-[0.7rem] text-gray-400">By subscribing you agree to our Terms & Conditions and Privacy & Cookies Policy.</p>
         </div>
 
-        <div>
-          <h1>Need help ?</h1>
-          <span>(+800) 1234 5678 90</span>
-          <p>We are available 8:00am – 7:00pm</p>
+        <div className="">
+          <h1 className="text-[1.5rem] font-bold max-w-[350px]">Need help?</h1>
+          <span className="text-[1.5rem] font-bold max-w-[350px]">(+800) 1234 5678 90</span>
+          <p className="pt-4 pb-4 text-[0.85rem] text-gray-400">We are available 8:00am – 7:00pm</p>
           <div className="flex items-center gap-2">
             <button className="flex items-center text-white bg-black border border-gray-500 rounded-[10px] p-1">
               <div>
@@ -42,7 +42,7 @@ const Contact = () => {
               </div>
             </button>
           </div>
-          <p>Shopping App: Try our View in Your Room feature, manage registries and save payment info.</p>
+          <p className="py-4 text-[0.7rem] text-gray-400">Shopping App: Try our View in Your Room feature, manage registries and save payment info.</p>
         </div>
       </div>
     </div>
