7ac780d31f979e647011ead8dfb017f4f341839b
danie odukwe <kingdanny295@gmail.com>
2024-01-08 14:00:53 +0100

    added the related product container

--- frontend/src/components/RelatedProducts.jsx
+++ frontend/src/components/RelatedProducts.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-import Card from '../ui/Card'
-import { Pagination, A11y } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper-bundle.css';
 import { FaStar } from "react-icons/fa";
@@ -8,6 +6,8 @@ import { IoIosHeartEmpty } from "react-icons/io";
 import { LiaCompressSolid } from "react-icons/lia";
 import { BsArrowRepeat } from "react-icons/bs";
 import { IoBagOutline } from "react-icons/io5";
+
+import { womenRelatedProducts } from '../constants';
 
 const RelatedProducts = () => {
 
@@ -31,10 +31,59 @@ const RelatedProducts = () => {
     setActiveImageIndices((prev) => ({ ...prev, [id]: newIndex }));
   };
 
-  
   return (
     <div>
       <h1 className="text-[1.2rem] font-medium py-3 border-b border-gray-300">Related products</h1>
+      <div className="relative">
+            <Swiper
+            // install Swiper modules
+            spaceBetween={50}
+            slidesPerView={1}
+            pagination={{ clickable: true }}
+            speed={2000}
+            parallax
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => console.log(swiper)}
+          >
+            <SwiperSlide>
+              <div className="flex overflow-x-auto gap-8 holder relative py-10">
+              {womenRelatedProducts.map((item) => (
+                <div
+                  key={item.id}
+                  className="flex-none relative"
+                  onMouseMove={(event) => handleHover(item.id, event)}
+                >
+                  <div className="img-holder">
+                    <img
+                      className="object-cover w-[290px] h-[500px] cursor-pointer"
+                      src={item[`img${activeImageIndices[item.id] || 1}`]}
+                      alt={`product-${item.id}`}
+                    />
+                    {/* side functions */}
+                    <div className="sideBtns absolute top-5 right-5 text-[2.2rem] flex flex-col gap-3">
+                      <IoIosHeartEmpty className="p-2 bg-white rounded-full text-gray-600 hover:bg-red-500 hover:text-white hover:font-medium"/>
+                      <LiaCompressSolid className="p-2 bg-white rounded-full text-gray-600 hover:bg-red-500 hover:text-white hover:font-medium"/>
+                      <BsArrowRepeat className="p-2 bg-white rounded-full text-gray-600 hover:bg-red-500 hover:text-white hover:font-medium"/>
+                      <IoBagOutline className="p-2 bg-white rounded-full text-gray-600 hover:bg-red-500 hover:text-white hover:font-medium"/>
+                    </div>
+                  </div>
+                  <div className="bg-white flex flex-col gap- my-3">
+                    <div className="flex  items-center gap-3"><FaStar className="text-[0.8rem] text-yellow-400"/> <span className="text-[0.8rem] font-medium">{item?.review}</span></div>
+                    <p className="text-[0.8rem] capitalize">{item?.name}</p>
+                    <div className="flex items-center gap-5 text-[0.9rem]">
+                      <span className="text-gray-400 line-through">{item?.oldPrice}</span>
+                      <span>{item?.price}</span>
+                    </div>
+                  </div>
+                  <div className="absolute top-4 left-2  px-2 py-1 text-[0.7rem] bg-white text-green-500 font-medium">
+                    {item.tag}
+                  </div>
+                </div> 
+              ))}
+              </div>
+            </SwiperSlide>
+        </Swiper>
+          </div>
     </div>
   )
 }
