01d78abcc76b1368c1be20985354f21a06d52a17
danie odukwe <kingdanny295@gmail.com>
2024-01-10 14:08:21 +0100

    created the cart component

--- frontend/src/ui/Cart.jsx
+++ frontend/src/ui/Cart.jsx
@@ -1,8 +1,36 @@
+import cloth from '../assets/arizona1.jpeg'
 
 const Cart = () => {
   return (
     <div>
-      
+      <div>
+        <div>
+          <img src={cloth} alt=""/>
+          <div>
+            <span>Basic Colored Sweatpants With Elastic Hems</span>
+            <div>
+              <span>1</span>
+              <span>x</span>
+              <span>$19.90</span>
+            </div>
+            <div>Color: Black</div> <div>Size: M</div>
+          </div>
+        </div>
+
+        <div>
+          <div>
+            <span>Subtotal</span>
+            <span>$19.90</span>
+          </div>
+          <p>You have 1 item in your cart</p>
+          <p>RESET</p>
+        </div> 
+
+        <div>
+          <button>View cart</button>
+          <button>Checkout</button>
+        </div>
+      </div>
     </div>
   )
 }
