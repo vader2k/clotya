af8680c7ca2d3f242685bed9ff3dd2145c4624e5
danie odukwe <kingdanny295@gmail.com>
2024-01-10 14:43:48 +0100

    added the route

--- frontend/src/routes/Routers.jsx
+++ frontend/src/routes/Routers.jsx
@@ -1,5 +1,8 @@
 import { Routes , Route } from "react-router-dom"
 import { Accessories, Blog, Contact, Home, Products, Details } from "../pages"
+import Clothing from "../pages/News/Clothing"
+import Collection from "../pages/News/Collection"
+import Fashion from '../pages/News/Fashion'
 
 const Router = () => {
   return (
@@ -11,6 +14,9 @@ const Router = () => {
     <Route path="/contact" element={<Contact/>}/>
     <Route path="/products/:id" element={<Products/>}/>
     <Route path="/details" element={<Details/>}/>
+    <Route path="clothing" element={<Clothing/>}/>
+    <Route path="collection" element={<Collection/>}/>
+    <Route path="fashion" element={<Fashion/>}/>
     </Routes>
     </div>
   )
