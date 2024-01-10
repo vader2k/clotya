193c76fb0a0efe54f5d16144f0a724fc4c0db2f0
danie odukwe <kingdanny295@gmail.com>
2024-01-10 14:24:40 +0100

    added cart icons

--- frontend/src/ui/Cart.jsx
+++ frontend/src/ui/Cart.jsx
@@ -1,9 +1,11 @@
 import cloth from '../assets/arizona1.jpeg'
+import { MdClose } from "react-icons/md";
+
 
 const Cart = () => {
   return (
-    <div className='absolute right-[100px] top-[80px] z-50 bg-white'>
-      <div>
+    <div className='absolute right-[100px] top-[80px] z-50 bg-white border rounded-md'>
+      <div className='flex flex-col gap-5 px-3 py-5'>
         <div>
           <img className='h-[80px] object-contain' src={cloth} alt=""/>
           <div>
@@ -14,6 +16,9 @@ const Cart = () => {
               <span>$19.90</span>
             </div>
             <div>Color: Black</div> <div>Size: M</div>
+          </div>
+          <div>
+
           </div>
         </div>
 
