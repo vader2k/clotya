9284c5a63b329ab3c7c623cbdee01d5baae73d12
danie odukwe <kingdanny295@gmail.com>
2024-01-20 08:06:01 +0100

    added the remove item and reset cart button function

--- frontend/src/ui/Cart.jsx
+++ frontend/src/ui/Cart.jsx
@@ -5,6 +5,8 @@ import { useDispatch } from "react-redux";
 
 const Cart = () => {
 
+  const dispatch = useDispatch()
+
   const products = useSelector(state=> state.cart.products)
 
   const totalPrice = () => {
@@ -31,7 +33,7 @@ const Cart = () => {
                 </div>
               </div>
               <div>
-                <RiDeleteBin5Line className='text-red-500 cursor-pointer'/>
+                <RiDeleteBin5Line className='text-red-500 cursor-pointer' onClick={()=>dispatch(removeItem)}/>
               </div>
             </div>
           ))
@@ -42,7 +44,7 @@ const Cart = () => {
             <span className='text-[1rem] font-bold text-red-500'>${totalPrice()}</span>
           </div>
           <p className='text-[0.85rem]'>You have {products.length} item in your cart</p>
-          <p className='text-[0.75rem] text-red-500 cursor-pointer font-bold'>RESET</p>
+          <p className='text-[0.75rem] text-red-500 cursor-pointer font-bold' onClick={()=> dispatch(resetCart)}>RESET</p>
         </div> 
 
         <div className='flex flex-col gap-2 text-[0.85rem] text-white'>
