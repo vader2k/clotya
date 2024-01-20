5d9b39d487df54c9d3f402ca5a8588bb0dfbe610
danie odukwe <kingdanny295@gmail.com>
2024-01-20 06:48:21 +0100

    removed unnecesary wraper from my main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,17 +4,14 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { persistor, store } from './Redux/store'
-import { PersistGate } from 'redux-persist/integration/react'
+import { store } from './Redux/store'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <PersistGate loading={"loading"} persistor={persistor}>
           <App/>
-        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
