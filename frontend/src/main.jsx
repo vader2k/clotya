129f4f1ccb0cd0941fe08b673adf5c72ba27f066
danie odukwe <kingdanny295@gmail.com>
2024-01-20 08:44:25 +0100

    added persistor to keep my cart items even after refreshing my page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,14 +4,16 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { store } from './Redux/store'
-
+import { store, persistor } from './Redux/store'
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
+        <PersistGate loading={'loading'} persistor={persistor}>
           <App/>
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
