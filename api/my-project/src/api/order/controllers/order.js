253ba0904cd21c3d8a2b2f10aa6e96464965a6e6
danie odukwe <kingdanny295@gmail.com>
2024-01-23 16:15:46 +0100

    tried making my payment gateway work

--- api/my-project/src/api/order/controllers/order.js
+++ api/my-project/src/api/order/controllers/order.js
@@ -1,60 +1,55 @@
-const product = require('../../product/controllers/product');
-
 const stripe = require('stripe')(process.env.STRIPE_KEY);
 
 'use strict';
-/**
- * order controller
- */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::order.order', ({strapi}) => ({
-    async create(ctx) {
-        const { email, products } = ctx.request.body;
+module.exports = createCoreController('api::order.order', ({ strapi }) => ({
+  async create(ctx) {
+    const { email, products } = ctx.request.body;
 
-        const lineItmes = await Promise.all(
-            products.map(async (product) => 
-                { const item = await strapi
-                    .service("api::product.product")
-                    .findOne(product.id)
+    const lineItems = await Promise.all(
+        products && Array.isArray(products)
+            ? products.map(async (product) => {
+                  const item = await strapi.service("api::product.product").findOne(product.id);
+    
+                  return {
+                      price_data: {
+                          currency: "usd",
+                          product_data: {
+                              name: item.title,
+                          },
+                          unit_amount: item.price * 100,
+                      },
+                      quantity: item.quantity,
+                      colors: item.colors,
+                      sizes: item.sizes,
+                  };
+              })
+            : []
+    );
 
-                    return {
-                        price_data: {
-                            currency: "usd",
-                            product_data: {
-                                name: item.title,
-                            },
-                            unit_amount: item.price * 100,
-                        },
-                        quantity: item.quantity,
-                        colors: item.colors,
-                        sizes: item.sizes
-                    }
-                }
-            )
-        )
-        try {
-            const session = await stripe.checkout.sessions.create({
-                mode: 'payment',
-                success_url: `${process.env.CLIENT_URL}?success=true`,
-                cancel_url: `${process.env.CLIENT_URL}?success=false`,
-                lineItmes: lineItmes,
-                shipping_address_collection: { allowed_countries: ["US", "CA"]},
-                payment_method_types: ["card"]
-            });
+    try {
+      const session = await stripe.checkout.sessions.create({
+        mode: 'payment',
+        success_url: `${process.env.CLIENT_URL}?success=true`,
+        cancel_url: `${process.env.CLIENT_URL}?success=false`,
+        line_items: lineItems, // Fix typo: lineItmes to line_items
+        shipping_address_collection: { allowed_countries: ['US', 'CA'] },
+        payment_method_types: ['card'],
+      });
 
-            await strapi.service("api::order:order").create({
-                data: {
-                    products,
-                    stripeId: session.id
-                },
-            })
+      await strapi
+        .service('api::order.order')
+        .create({
+          products,
+          stripeId: session.id,
+        });
 
-            return { stripeSession: session };
-        } catch (error) {
-            ctx.respond.status = 500;
-            return error
-        }
+      return { stripeSession: session };
+    } catch (error) {
+      ctx.response.status = 500; // Fix typo: ctx.respond.status to ctx.response.status
+      return error;
     }
-}))
+  },
+}));
